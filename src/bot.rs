519e7a5d1bc6fc85e02a0597110f3db3804bd4af
Shelvacu <shelvacu@gmail.com>
2020-04-26 18:52:16 -0700

    move old_is_win

--- src/bot.rs
+++ src/bot.rs
@@ -866,25 +866,6 @@ const NO_WORDS :&'static[&'static str] = &[
 const ZERO_WORDS:&'static[&'static str] = &["zero", "zerovote", "nil", "nada", "nothing"];
 const IGNORE_WORDS:&'static[&'static str] = &["in", "i", "I", "think", "say", "fuck"];
 
-fn old_is_win(yes_votes:i64, no_votes:i64, is_super:bool) -> bool {
-  if is_super {
-      let total = yes_votes + no_votes;
-      let div = total / 3;
-      let rem = total % 3;
-      // 10 = 3 rem 1
-      // win is >= 7 (div*2+rem)
-      // 11 = 3 rem 2
-      // win is >= 7 (div*2+rem)
-      // 12 = 4 rem 0
-      // 8 is a "tie", so lose
-      // win is >= 9 (div*2+rem)+1
-      let winning_amount = (div*2+rem) + if rem == 0 {1} else {0};
-      return yes_votes >= winning_amount;
-  }else{
-      return yes_votes > no_votes;
-  }
-}
-
 fn is_win(yes_votes:i64, no_votes:i64, is_super:bool) -> bool {
     if is_super {
         return (yes_votes/2, yes_votes%2) > (no_votes, 0);
@@ -1146,11 +1127,30 @@ fn vote_common(
     return Cow::Borrowed("Vote cast");
 }
 
+#[cfg(test)]
 mod test {
+    fn old_is_win(yes_votes:i64, no_votes:i64, is_super:bool) -> bool {
+      if is_super {
+          let total = yes_votes + no_votes;
+          let div = total / 3;
+          let rem = total % 3;
+          // 10 = 3 rem 1
+          // win is >= 7 (div*2+rem)
+          // 11 = 3 rem 2
+          // win is >= 7 (div*2+rem)
+          // 12 = 4 rem 0
+          // 8 is a "tie", so lose
+          // win is >= 9 (div*2+rem)+1
+          let winning_amount = (div*2+rem) + if rem == 0 {1} else {0};
+          return yes_votes >= winning_amount;
+      }else{
+          return yes_votes > no_votes;
+      }
+    }
+
     #[test]
     fn wins_match(){
         use super::is_win;
-        use super::old_is_win;
         assert_eq!(old_is_win(1, 0, false), is_win(1, 0, false));
         assert_eq!(old_is_win(1, 1, false), is_win(1, 1, false));
         assert_eq!(old_is_win(1, 2, false), is_win(1, 2, false));
