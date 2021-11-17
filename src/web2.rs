12fcbe0b4c4254d090df4bcd15a8f2c24ed8e6a8
Shelvacu <github@shelvacu.com>
2021-11-17 15:09:11 -0800

    Add shortlink handler. Bump version to 1.0.1. This is sentimental versioning, in case you couldn't tell.

--- src/web2.rs
+++ src/web2.rs
@@ -1557,6 +1557,36 @@ fn motions_api_compat(
     Content(ContentType::JSON, serde_json::to_string(&res).unwrap())
 }
 
+// Intended for nginx to internally redirect from shortlink domains, but if someone goes here directly that's fine
+#[get("/shortlink/<damm_id>")]
+fn shortlink(
+    damm_id: String,
+    ctx: CommonContext,
+) -> impl Responder<'static> {
+    let id:i64;
+    if let Some(digits) = crate::damm::validate_ascii(damm_id.as_str()) {
+        id = atoi::atoi(digits.as_slice()).unwrap();
+    } else {
+        return None;
+    }
+
+    use schema::motions::dsl as mdsl;
+    use schema::auctions::dsl as adsl;
+    use diesel::dsl::{select, exists};
+
+    let is_motion = select(exists(mdsl::motions.filter(mdsl::rowid.eq(id)))).get_result(&*ctx).unwrap();
+    if is_motion {
+        return Some(Redirect::permanent(uri!(motion_listing: damm_id = damm_id)));
+    }
+
+    let is_auction = select(exists(adsl::auctions.filter(adsl::rowid.eq(id)))).get_result(&*ctx).unwrap();
+    if is_auction {
+        return Some(Redirect::permanent(uri!(auction_view:   damm_id = damm_id)));
+    }
+
+    None
+}
+
 pub fn main() {
     rocket::ignite()
         .manage(rocket_diesel::init_pool())
@@ -1576,6 +1606,7 @@ pub fn main() {
             auction_index,
             auction_bid,
             auction_view,
+            shortlink,
         ])
         .launch();
 }
